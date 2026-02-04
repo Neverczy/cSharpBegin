2a64254ffc9987180810046e84d9c4db4f3e42ae
chenzy <chenzy940829@outlook.com>
2026-02-05 01:20:25 +0800

    2026/2/5/1:19 vs commit - whileTest

---
+++ switchcase/Program.cs
@@ -0,0 +1,120 @@
+﻿namespace switchcase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 绩效工资
+            //Console.Write("输入课程评级:");
+            //string rate = Console.ReadLine();
+            //int money = 4000;
+            //switch (rate)
+            //{
+            //    case "很兴奋":
+            //        Console.WriteLine("评价A级,绩效工资加500");
+            //        money += 500;
+            //        break;
+            //    case "很充实":
+            //        Console.WriteLine("评级B级,无绩效工资");
+            //        break;
+            //    case "还好吧":
+            //        Console.WriteLine("评级C级,绩效工资扣300");
+            //        money -= 300;
+            //        break;
+            //    case "难理解":
+            //        Console.WriteLine("评级D级,绩效工资扣500");
+            //        money -= 500;
+            //        break;
+            //    case "枯燥乏味":
+            //        Console.WriteLine("评级E级,绩效工资扣800");
+            //        money -= 800;
+            //        break;
+            //    default:
+            //        Console.WriteLine("输入有误!");
+            //        break;
+            //}
+            //Console.WriteLine($"工资为 : {money}");
+            #endregion
+            #region 买星巴克
+            //Console.WriteLine("购买星巴克咖啡");
+            //Console.WriteLine("1.中杯 $5");
+            //Console.WriteLine("2.大杯 $7");
+            //Console.WriteLine("3.超大杯 $11");
+            //Console.Write("请输入购买大小:");
+            //string tp = Console.ReadLine();
+            //int money = 10;
+            //switch (tp)
+            //{
+            //    case "1":
+            //        if (money >= 5)
+            //        {
+            //            Console.WriteLine("购买中杯成功,花费5元!");
+            //            money -= 5;
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("钱不够,请购买其他大小");
+            //        }
+            //            break;
+            //    case "2":
+            //        if (money >= 7)
+            //        {
+            //            Console.WriteLine("购买大杯成功,花费7元!");
+            //            money -= 7;
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("钱不够,请购买其他大小");
+            //        }
+            //        break;
+            //    case "3":
+            //        if (money >= 11)
+            //        {
+            //            Console.WriteLine("购买超大杯成功,花费11元!");
+            //            money -= 11;
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("钱不够,请购买其他大小");
+            //        }
+            //        break;
+            //    default:
+            //        Console.WriteLine("没有此大小,请重新输入!");
+            //        break;
+            //}
+            //Console.WriteLine($"剩余金钱 : {money}");
+            #endregion
+            #region 成绩
+            Console.Write("请输入成绩(0-100) : ");
+            try
+            {
+                int score = int.Parse(Console.ReadLine());
+                score /= 10;
+                switch (score)
+                {
+                    case 10:
+                    case 9:
+                        Console.WriteLine("评级为A");
+                        break;
+                    case 8:
+                        Console.WriteLine("评级为B");
+                        break;
+                    case 7:
+                        Console.WriteLine("评级为C");
+                        break;
+                    case 6:
+                        Console.WriteLine("评级为D");
+                        break;
+                    default:
+                        Console.WriteLine("评级为E");
+                        break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("请输入数字!!");
+            }
+            #endregion
+        }
+    }
+}
---
+++ whileCricle/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+namespace whileCricle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //while (true)
+            //{
+            //    //unsafe
+            //    //{
+            //    //    byte* buff;
+            //    //    buff = (byte*)Marshal.AllocHGlobal(1000);
+            //    //}
+            //}
+            #region 1.控制台输出1-100
+            //int i1 = 1;
+            //while (i1 <= 100)
+            //{
+            //    Console.WriteLine(i1);
+            //    i1++;
+            //}
+            #endregion
+
+            #region 2.控制台输出1-100总和
+            //int i2 = 1;
+            //int sum = 0;
+            //while (i2 <= 100)
+            //{
+            //    sum += i2;
+            //    i2++;
+            //}
+            //Console.WriteLine("总和为{0}",sum);
+            #endregion
+
+            #region 3.计算1-100除了能被7整除之外所有的和
+            //int i3 = 0;
+            //int sum = 0;
+            //while (i3 < 100)
+            //{
+            //    i3++;
+            //    if (i3 % 7 == 0)
+            //    {
+            //        continue;
+            //    }
+            //    sum += i3;
+            //}
+            //Console.WriteLine("拆除7整除后总和为 : {0}", sum);
+            //4315
+            #endregion
+
+            #region 4.找素数
+            Console.Write("带你找素数,请输入一个数 : ");
+            try
+            {
+                int num4 = int.Parse(Console.ReadLine());
+                int i4 = 2;
+                while (i4 < num4)
+                {                 
+                    if (num4 % i4 == 0 )
+                    {
+                        break;
+                    }
+                    i4++;
+                }
+                if (i4 == num4)
+                {
+                    Console.WriteLine($"{num4}是素数");
+                }
+                else
+                {
+                    Console.WriteLine($"{num4}不是素数");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("输入错误,请输入一个数字!");
+            }
+            #endregion
+
+            #region 输入账号密码admin/8888
+            //while (true)
+            //{
+            //    Console.Write("请输入账号 : ");
+            //    string acc = Console.ReadLine();
+            //    Console.Write("请输入密码 : ");
+            //    string pwd = Console.ReadLine();
+            //    if(acc =="admin" && pwd == "8888")
+            //    {
+            //        Console.WriteLine("登录成功!");
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("用户名或密码错误,请重新登录!");
+            //    }
+            //}
+            #endregion
+        }
+    }
+}
